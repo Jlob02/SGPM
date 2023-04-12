af1ce1ceda73132e58a40d6f56a7327514f3ee3b
Joaquim <joaquim.jojo30@hotmail.com>
2023-04-12 11:24:32 +0100

    sprint 1.3

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -15,18 +15,17 @@
     <div class="container-fluid">
         <!--navbar-->
         <header class="row _navbar ">
-            <div class="col-7"></div>
-            <div class="col-5 text-white  p-2 d-flex justify-content-around align-items-center">
+            <div class="col-12 text-white  p-2 d-flex justify-content-end align-items-center">
                 @auth
-                <span>Olá {{Auth::user()->u_nome}}</span> |
-                <a href="/perfil" class="text-white d-flex align-items-center"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="me-2 bi bi-person-fill" viewBox="0 0 16 16">
+                <span class="ms-2 me-2">Olá {{Auth::user()->u_nome}}</span> |
+                <a href="/perfil" class="text-white d-flex align-items-center ms-2 me-2"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="me-2 bi bi-person-fill" viewBox="0 0 16 16">
   <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
 </svg>  Meu perfil</a> |
-                <a href="/logout" class="text-white d-flex align-items-center"> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class=" me-2 bi bi-box-arrow-left" viewBox="0 0 16 16">
+                <a href="/logout" class="text-white d-flex align-items-center ms-2 me-2"> <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class=" me-2 bi bi-box-arrow-left" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z"/>
   <path fill-rule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z"/>
 </svg> Sair</a> |
-                <ul class="navbar-nav">
+                <ul class="navbar-nav ms-2 me-2 ">
                     <li class="nav-item dropdown">
                         <button class="btn text-white dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                             PT
