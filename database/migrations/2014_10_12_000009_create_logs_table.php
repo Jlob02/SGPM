964b1a349e3cab386e1170d47e096b48d584003b
Joaquim Lobo <77938891+Jlob02@users.noreply.github.com>
2023-06-10 12:57:34 +0100

    correção de erro nas migrations

--- database/migrations/2014_10_12_000009_create_logs_table.php
+++ database/migrations/2014_10_12_000009_create_logs_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('codigo', function (Blueprint $table) {
+        Schema::create('logs', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
